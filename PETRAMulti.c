df8fc155fad264c3ff1737eb92c0171adc62c53b
hysmun <throdlemassif@hotmail.com>
2017-03-22 09:11:11 +0100

    includes
    includes

--- PETRAMulti.c
+++ PETRAMulti.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <process.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
 
 
 int main(int argc, char *argv[])
